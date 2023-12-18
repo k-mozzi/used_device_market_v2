9c9a68b55a71fd03fe21a90c20a6410c2594df91
An-sangdo <akdhrtl1@naver.com>
2023-12-18 14:22:50 +0900

    1111

--- src/main/java/teamproject/usedmarket/web/member/MemberController.java
+++ src/main/java/teamproject/usedmarket/web/member/MemberController.java
@@ -1,48 +1,56 @@
-package teamproject.usedmarket.web.member;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import teamproject.usedmarket.domain.login.LoginService;
-import teamproject.usedmarket.domain.member.Member;
-
-import javax.validation.Valid;
-
-@Slf4j
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("/members")
-public class MemberController {
-
-    private final LoginService loginService;
-
-    @GetMapping("/add")
-    public String addForm(@ModelAttribute("member") Member member) {
-        return "members/addMemberForm";
-    }
-
-    @PostMapping("/add")
-    public String join(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
-
-        try {
-            if (bindingResult.hasErrors()) {
-                log.info("error={}", bindingResult);
-                return "members/addMemberForm";
-            }
-
-            loginService.join(member);
-            return "home";
-        } catch (IllegalStateException e) {
-            bindingResult.reject("join fail", e.getMessage());
-            log.error("중복 아이디 예외 발생: {}", e.getMessage());
-            return "members/addMemberForm";
-        }
-
-    }
-}
+//package teamproject.usedmarket.web.member;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import teamproject.usedmarket.domain.login.LoginService;
+//import teamproject.usedmarket.domain.member.Member;
+//
+//import javax.validation.Valid;
+//import java.util.List;
+//
+//@Slf4j
+//@Controller
+//@RequiredArgsConstructor
+//@RequestMapping("/members")
+//public class MemberController {
+//
+//    private final MemberService memberService;
+//
+//    @GetMapping("/members/new")
+//    public String createForm(Model model) {
+//        model.addAttribute("memberForm", new MemberForm());
+//        return "members/createMemberForm";
+//    }
+//
+//    @PostMapping("/members/new")
+//    public String create(@Valid MemberForm form, BindingResult result) {
+//
+//        if (result.hasErrors()) {
+//            return "/members/createMemberForm";
+//        }
+//
+//        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+//
+//        Member member = new Member();
+//        member.setName(form.getName());
+//        member.setAddress(address);
+//
+//        memberService.join(member);
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/members")
+//    public String list(Model model) {
+//        List<Member> members = memberService.findMembers();
+//        model.addAttribute("members", members);
+//        return "members/memberList";
+//    }
+//}
