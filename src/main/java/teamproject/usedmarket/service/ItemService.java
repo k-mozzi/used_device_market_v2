9b15d98396d63912006a9c4d566c59798324980e
k-mozzi <kimkm0405@gmail.com>
2023-12-20 18:05:27 +0900

    아이템 타입 구현

--- src/main/java/teamproject/usedmarket/service/ItemService.java
+++ src/main/java/teamproject/usedmarket/service/ItemService.java
@@ -1,14 +1,16 @@
 package teamproject.usedmarket.service;
 
+import org.springframework.web.multipart.MultipartFile;
 import teamproject.usedmarket.domain.item.Item;
 import teamproject.usedmarket.repository.ItemUpdateDto;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 public interface ItemService {
 
-    Item save(Item item);
+    void save(Item item, MultipartFile file) throws IOException;
 
     void update(Long itemId, ItemUpdateDto updateParam);
 
--- src/main/java/teamproject/usedmarket/web/item/ItemController.java
+++ src/main/java/teamproject/usedmarket/web/item/ItemController.java
@@ -4,13 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.multipart.MultipartFile;
 import teamproject.usedmarket.domain.item.Item;
+import teamproject.usedmarket.domain.item.ItemType;
 import teamproject.usedmarket.domain.item.SaleStatus;
-import teamproject.usedmarket.domain.member.Region;
-import teamproject.usedmarket.repository.ItemUpdateDto;
 import teamproject.usedmarket.service.ItemService;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 
@@ -20,6 +21,7 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
+
 
     @GetMapping
     public String items(Model model) {
@@ -38,12 +40,16 @@ public class ItemController {
     @GetMapping("/add")
     public String addForm(@ModelAttribute Item item, Model model) {
         model.addAttribute("statuses", SaleStatus.values());
+        model.addAttribute("itemTypes", ItemType.values());
         return "item/addForm";
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item) {
-        Item savedItem = itemService.save(item);
+    public String addItem(@ModelAttribute Item item,@RequestParam("file") MultipartFile file) throws IOException {
+
+        item.setCreateDatetime(new Date());
+        item.setUpdateDatetime(new Date());
+        itemService.save(item, file);
         return "redirect:/items";
     }
 
@@ -55,8 +61,8 @@ public class ItemController {
     }
 
 //    @PostMapping("/{itemId}/edit")
-//    public String edit(@PathVariable Long itemId, @ModelAttribute ItemUpdateDto updateParam) {
-//        itemService.update(itemId, updateParam);
+//    public String edit(@PathVariable Long itemId, @ModelAttribute ItemUpdateDto updateParam, MultipartFile file) {
+//        itemService.update(itemId, updateParam, file);
 //        return "redirect:item/items/{itemId}";
 //    }
 
