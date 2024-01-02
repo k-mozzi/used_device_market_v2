d045e1f5a6702271092ec5002690fa0505ff31de
k-mozzi <kimkm0405@gmail.com>
2024-01-02 14:39:37 +0900

    아이템 목록에 카테고리 셀렉트박스 추가

--- src/main/java/teamproject/usedmarket/repository/ItemRepository.java
+++ src/main/java/teamproject/usedmarket/repository/ItemRepository.java
@@ -4,6 +4,7 @@ import teamproject.usedmarket.domain.item.Item;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface ItemRepository {
@@ -18,18 +19,18 @@ public interface ItemRepository {
 
     public void update(Long itemId, ItemUpdateDto updateParam);
 
-    void incrementViewsCount(Long itemId);
+    public void incrementViewsCount(Long itemId);
 
     public void delete(Long itemId);
 
-    public List<Item> findItemsWithPaging(int page, int pageSize);
+    public List<Item> findItemsWithPaging(int page, int pageSize, String itemType);
 
     public int countItems();
 
-    public List<Item> findItemsSortedByRegistrationDate(int page, int pageSize);
+    public List<Item> findItemsSortedByRegistrationDate(int page, int pageSize, String itemType);
 
-    public List<Item> findItemsSortedByViewsCount(int page, int pageSize);
+    public List<Item> findItemsSortedByViewsCount(int page, int pageSize, String itemType);
 
-    public List<Item> findItemsSortedByLikesCount(int page, int pageSize);
+    public List<Item> findItemsSortedByLikesCount(int page, int pageSize, String itemType);
 
 }
