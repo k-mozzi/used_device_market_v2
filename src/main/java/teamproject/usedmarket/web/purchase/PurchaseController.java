2caa3a6ddcdc88ae819c4940dc1433a9e114f943
heeee1b <86914189+An-sangdo@users.noreply.github.com>
2024-01-10 15:13:16 +0900

    Update PurchaseController.java

--- src/main/java/teamproject/usedmarket/web/purchase/PurchaseController.java
+++ src/main/java/teamproject/usedmarket/web/purchase/PurchaseController.java
@@ -6,17 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import teamproject.usedmarket.domain.item.Item;
-import teamproject.usedmarket.repository.ItemUpdateDto;
+import teamproject.usedmarket.web.item.ItemUpdateDto;
 import teamproject.usedmarket.service.item.ItemService;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 @Slf4j
