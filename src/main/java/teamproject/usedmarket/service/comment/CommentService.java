62c383aca68e4bf552415d72dd205f74112f0a12
heeee1b <86914189+An-sangdo@users.noreply.github.com>
2024-01-10 15:03:51 +0900

    Update CommentService.java

--- src/main/java/teamproject/usedmarket/service/comment/CommentService.java
+++ src/main/java/teamproject/usedmarket/service/comment/CommentService.java
@@ -2,7 +2,7 @@ package teamproject.usedmarket.service.comment;
 
 import org.apache.ibatis.annotations.Param;
 import teamproject.usedmarket.domain.comment.Comment;
-import teamproject.usedmarket.repository.CommentUpdateDto;
+import teamproject.usedmarket.web.item.CommentUpdateDto;
 
 import java.util.List;
 
